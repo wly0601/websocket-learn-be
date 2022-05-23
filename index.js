83d83407bbfb72826e74b50f18eb486796e3cf70
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2022-05-23 10:12:36 +0700

    feat: socket.io on express

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const { Server } = require("socket.io");
+const app = require("./app");
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("INFO:", "seseorang telah bergabung ke chat room!");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("INFO:", "seseorang telah pergi dari chat room!");
+  });
+});
+
+server.listen(8000, () => {
+  console.log("INFO:", "Listening on port 8000");
+});
